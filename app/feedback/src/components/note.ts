09db4423686aee31f571e08e1a16928e5f443419
cmak2 <35709929+cmak2@users.noreply.github.com>
2019-05-29 12:09:59 -0600

    Fixed Mixed tabs/spaces

--- app/feedback/src/components/note.ts
+++ app/feedback/src/components/note.ts
@@ -1,7 +1,7 @@
 export interface Note {
-	author: string;
-	content: string;
-	type: string;
+    author: string;
+    content: string;
+    type: string;
     timestamp: number;
     x: number;
     y: number;
